86074fce558b81524c908c3fb945ad35b7cc995c
meifakun <runner.mei@gmail.com>
2018-08-21 08:34:34 +0800

    fix mssql tests

--- tests/user.go
+++ tests/user.go
@@ -104,6 +104,13 @@ type TestUserGroups interface {
 	//          WHERE groups.id = #{id}
 	//          GROUP BY groups.id
 	//         -- see JSON_OBJECTAGG and JSON_ARRAYAGG
+	//
+	// @mssql SELECT groups.id, groups.name, CONCAT('[', STRING_AGG(CASE(u2g.user_id AS varchar(100))，','), ']') as user_ids
+	//          FROM gobatis_usergroups as groups LEFT JOIN gobatis_user_and_groups as u2g
+	//               ON groups.id = u2g.group_id
+	//          WHERE groups.id = #{id}
+	//          GROUP BY groups.id
+	//         -- see CROSS APPLY
 	Get(id int64) (*UserGroup, error)
 
 	Count() (int64, error)
