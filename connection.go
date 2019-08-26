c3517e847b01af98dab48016bc3ee1134eeb9bf0
meifakun <runner.mei@gmail.com>
2019-08-26 18:28:11 +0800

    fix DBRunner

--- connection.go
+++ connection.go
@@ -66,7 +66,7 @@ type Config struct {
 }
 
 type DBRunner interface {
-	Prepare(query string) (*sql.Stmt, error)
+	PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
 	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
 	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
