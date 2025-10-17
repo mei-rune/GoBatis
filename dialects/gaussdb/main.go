c4c3f2285d768e7dc629d07251946ba60231d76e
mfk <mfk@hengwei.com.cn>
2025-10-17 12:56:29 +0800

    fix bugs

--- dialects/gaussdb/main.go
+++ dialects/gaussdb/main.go
@@ -2,49 +2,51 @@ package gaussdb
 
 import (
 	"errors"
+	"strings"
 
+	"github.com/HuaweiCloudDeveloper/gaussdb-go/gaussdbconn"
 	"github.com/HuaweiCloudDeveloper/gaussdb-go/gaussdbtype"
 	"github.com/runner-mei/GoBatis/dialects"
 )
 
 func init() {
-	// dialects.SetHandleArray(dialects.Opengauss.Name(), handleError)
-	dialects.SetHandleArray(dialects.Opengauss.Name(), makePQArrayValuer, makePQArrayScanner)
+	dialects.SetHandleError(dialects.GaussDB.Name(), handleError)
+	dialects.SetHandleArray(dialects.GaussDB.Name(), makePQArrayValuer, makePQArrayScanner)
 }
 
-// func handleError(e error) error {
-// 	if e == nil {
-// 		return nil
-// 	}
+func handleError(e error) error {
+	if e == nil {
+		return nil
+	}
 
-// 	if pe, ok := e.(*pq.Error); ok {
-// 		switch pe.Code {
-// 		case "23505":
-// 			detail := strings.TrimPrefix(strings.TrimPrefix(pe.Detail, "Key ("), "键值\"(")
-// 			if pidx := strings.Index(detail, ")"); pidx > 0 {
-// 				return &dialects.Error{Validations: []dialects.ValidationError{
-// 					{Code: "unique_value_already_exists", Message: pe.Detail, Columns: strings.Split(detail[:pidx], ",")},
-// 				}, Err: e}
-// 			}
+	if pe, ok := e.(*gaussdbconn.GaussdbError); ok {
+		switch pe.Code {
+		case "23505":
+			detail := strings.TrimPrefix(strings.TrimPrefix(pe.Detail, "Key ("), "键值\"(")
+			if pidx := strings.Index(detail, ")"); pidx > 0 {
+				return &dialects.Error{Validations: []dialects.ValidationError{
+					{Code: "unique_value_already_exists", Message: pe.Detail, Columns: strings.Split(detail[:pidx], ",")},
+				}, Err: e}
+			}
 
-// 		case "42P01":
-// 			return dialects.ErrTableNotExists{
-// 				Err:       e,
-// 				Tablename: pe.Table,
-// 			}
+		case "42P01":
+			return dialects.ErrTableNotExists{
+				Err:       e,
+				Tablename: pe.TableName,
+			}
 
-// 		// case "23503":
-// 		//  return &Error{Validations: []ValidationError{
-// 		//    {Code: "PG.foreign_key_constraint", Message: pe.Message},
-// 		//  }, e: e}
-// 		default:
-// 			return &dialects.Error{Validations: []dialects.ValidationError{
-// 				{Code: "PG." + pe.Code.Name(), Message: pe.Message, Columns: []string{pe.Column}},
-// 			}, Err: e}
-// 		}
-// 	}
-// 	return e
-// }
+		// case "23503":
+		//  return &Error{Validations: []ValidationError{
+		//    {Code: "PG.foreign_key_constraint", Message: pe.Message},
+		//  }, e: e}
+		default:
+			return &dialects.Error{Validations: []dialects.ValidationError{
+				{Code: "GaussDB." + pe.Code, Message: pe.Message, Columns: []string{pe.ColumnName}},
+			}, Err: e}
+		}
+	}
+	return e
+}
 
 func makePQArrayValuer(v interface{}) (interface{}, error) {
 	switch a := v.(type) {
