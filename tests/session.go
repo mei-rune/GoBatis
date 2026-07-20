5eb8e99f47cd2f69845d9bb70a80e928d7415e28
mfk <mfk@hengwei.com.cn>
2026-07-20 10:58:27 +0800

    fix bugs

--- tests/session.go
+++ tests/session.go
@@ -20,7 +20,6 @@ import (
 	_ "github.com/runner-mei/GoBatis/dialects/pgx"
 	_ "github.com/runner-mei/GoBatis/dialects/pq"
 	_ "github.com/runner-mei/GoBatis/dialects/sqlite"
-	// _ "github.com/runner-mei/GoBatis/dialects/st_oscar"
 	// _ "github.com/SAP/go-hdb/driver"                  // sap hana
 	// _ "github.com/ibmdb/go_ibm_db"
 )
---
+++ tests/session_cgo.go
@@ -0,0 +1,8 @@
+// go:build cgo
+//   +build cgo
+
+package tests
+
+import (
+	_ "github.com/runner-mei/GoBatis/dialects/st_oscar"
+)
